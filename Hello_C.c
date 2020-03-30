8ee993125534a6d6b8eae4c99e71442d048b8ca0
MusaAkyuz <62879859+MusaAkyuz@users.noreply.github.com>
2020-03-30 13:25:17 +0300

    Add files via upload

---
+++ Hello_C.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //input output library is added
+
+int main () //main function is called
+{
+	printf("Hello C"); //hello c is written on the screen
+	return 0; //finish program
+}
